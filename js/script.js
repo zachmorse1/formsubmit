230666e0e1b5d7ee201312d328c1fcdf86e7a9d5
zachmorse1 <114124418+zachmorse1@users.noreply.github.com>
2023-01-12 11:41:27 +1000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
+
+function mobileMenu(x) {
+    x.classList.toggle("change");
+}
